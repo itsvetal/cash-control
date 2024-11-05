d8c91ed969f9db9c9d1117864b660ab360a3911f
iTsVetal <vitaliikryskiv.development@gmail.com>
2024-11-05 16:54:33 +0200

    Add new component Card.tsx, CostForm.tsx and NewCost.tsx V1.0.2

--- src/components/Costs/components/CostItem/CostItem.tsx
+++ src/components/Costs/components/CostItem/CostItem.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import './CostItem.css'
 import {CostItemProps} from "../../common/interfaces/CostItem. d";
-import CostDate from "./components/CostDate/CostDate";
+import CostDate from "../CostDate/CostDate";
+import Card from "../Card/Card";
 
 function CostItem(props: CostItemProps) {
     const {date, description , amount} = props.item;
 
     return (
-        <div className="cost-item">
+        <Card className='cost-item'>
             <CostDate date={date}/>
             <div className="cost-item__description">
                 <h2>{ description }</h2>
                 <div className="cost-item__price ">${ amount }</div>
             </div>
-        </div>
+        </Card>
     );
 }
 
